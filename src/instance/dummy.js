c94c5e0f47e82d02e04cd925002f4be9d29c1f7f
CHOI JAHYUN <0912jh@gmail.com>
2023-10-26 18:55:31 +0900

    first

--- src/instance/dummy.js
+++ src/instance/dummy.js
@@ -18,6 +18,21 @@ import React, { useState, useEffect } from "react";
 import React from "react";
 
 function dummy() {
+  async function getData() {
+    const url = "https://pixabay.com/api/";
+    const ceramic_key = "?key=13113157-b5ac5e3c3e3d3a0bc3c018098&q=yellow+flowers&image_type=photo";
+
+    try {
+      const response = await axios.get(`${url}${ceramic_key}`, {
+        params: {
+          largeImageURL
+        },
+      });
+    } catch (error) {
+      alert("서버정보에러발생");
+    }
+  }
+
   return <div></div>;
 }
 
