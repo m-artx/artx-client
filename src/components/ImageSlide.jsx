15bda063a924bda7c987e078c4c8524db5009a8b
CHOI JAHYUN <0912jh@gmail.com>
2023-11-01 13:07:46 +0900

    css 오류 공유

--- src/components/ImageSlide.jsx
+++ src/components/ImageSlide.jsx
@@ -32,7 +32,7 @@ const nextSlide = () => {}
   
 
   return ( 
-    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative">
+    <div className="max-w-[200px] h-[200px] w-full m-auto py-16 px-4 relative">
       <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className="w-full h-full bg-center bg-cover duration-500 rounded-2xl"></div>
 
     <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded=full p-2 bg-black/20 text-white cursor-pointer">
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import Anycom from "../components/Anycom";
+import React from 'react';
+import Anycom from '../components/Anycom';
+import SliderFirst from '../components/SliderFirst';
+import ImageSlide from '../components/ImageSlide';
 
 function MainPage() {
   return (
-    <div>
-      <input className="border rounded-full bg-black w-[320px] p-2 text-center placeholder-white" type="text" placeholder="검색어를 입력해주세요. 예) 작가명, 작품명"> 
-      </input>
-      <Anycom />
+    <div className="flex flex-col items-center">
+      <input
+        className="border rounded-full bg-black w-[320px] p-2 text-center placeholder-white "
+        type="text"
+        placeholder="검색어를 입력해주세요. 예) 작가명, 작품명"
+      ></input>
+      <div className="border border-red-800 flex-grow w-screen">
+         {/* 첫번째 이미지칸 */}
+         <div className="flex flex-col items-center py-2 w-full">
+          <p className="p-4">신작:新作</p>
+          <div className="flex border border-blue-800 w-screen h-[300px]">
+            {/* 이건 잘되는데? */}
+            <img className="w-[300px] h-[200px]" 
+            src="http://dafenart.co.kr/mall/shop_image/201702/20%2811%29.jpg"/>    
+            {/* 슬라이드왜이래? */}
+            <SliderFirst className="" />
+          </div>
+        </div>
+        {/* 두번째 이미지칸 */}
+        <div className="flex flex-col items-center py-2">
+          <p className="p-4">최근 판매된 작품</p>
+          <div className="flex border border-blue-800 w-screen h-[200px]">
+            <ImageSlide className="min-h-[200px]" />
+          </div>
+        </div>
+      
+      </div>
 
     </div>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
