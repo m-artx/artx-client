b1920efe139c81002eeebb17599b496eba2be07b
haeun jeon <yul__koo@naver.com>
2023-11-03 15:12:56 +0900

    "feat 스테이징 변경사항 "

--- src/App.js
+++ src/App.js
@@ -7,13 +7,10 @@ import Layout from './components/shared/Layout';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import Product from './pages/Product';
-<<<<<<< HEAD
 import ShoppingCart from './pages/ShoppingCart';
 import OrderHistory from './pages/OrderHistory';
-=======
-import ProductPage from "./pages/ProductPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
->>>>>>> aad94df38114e103e5c41cf14f2e2dce10180775
+import ProductPage from './pages/ProductPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 function App() {
   return (
@@ -28,16 +25,11 @@ function App() {
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/product" element={<Product />} />
-<<<<<<< HEAD
           <Route path="/cart" element={<ShoppingCart />} />
           <Route path="/orderhistory" element={<OrderHistory />} />
-=======
           <Route path="/productpage" element={<ProductPage />} />
           <Route path="/productdetail" element={<ProductDetailPage />} />
           <Route path="/productdetail/:productId" element={<ProductDetailPage />} />
-
-
->>>>>>> aad94df38114e103e5c41cf14f2e2dce10180775
         </Route>
       </Routes>
     </div>
