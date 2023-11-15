6f416b0d62a3180e7a7d114d2c2a06f5f331d75e
haeun jeon <yul__koo@naver.com>
2023-11-15 16:35:26 +0900

    Feature/ARTX-88 feat:작가센터 구현

--- src/pages/NoticePage.jsx
+++ src/pages/NoticePage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function NoticePage() {
-  return (
-    <div>공지사항
-      <div>게시판 내용</div>
-    </div>
-  )
+    return (
+        <div>
+            <h2>고객센터</h2>
+            공지사항
+            <div>게시판 내용</div>
+        </div>
+    );
 }
 
-export default NoticePage
+export default NoticePage;
