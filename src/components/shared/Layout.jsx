2c18b4b8982e91490dfc56176de0ca096d4219ff
CHOI JAHYUN <0912jh@gmail.com>
2023-11-17 21:19:30 +0900

    ARTX-110 feat: Login fixing

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -4,14 +4,13 @@ import Header from '../Header';
 
 export default function Layout() {
    return (
-      <div className="flex flex-col items-center h-screen max-w-screen text-white  border-box font-sans border text-sm">
-         <div className="flex justify-center h-[140px] max-w-[1300px] min-h-[140px] border">
+      <div className="flex flex-col items-center h-screen max-w-screen bg-black text-white  border-box font-sans border text-sm">
+         <div className="flex justify-center h-[140px] max-w-[1500px] min-h-[140px] border">
             <Header />
          </div>
-         <div className="flex justify-center border border-blue-500 h-screen-140 max-w-[1300px]"
-         >{<Outlet />}</div>
+         <div className="flex justify-center border border-blue-500 h-screen-140  ">{<Outlet />}</div>
          {/* Outlet는 중첩된 라우트의 자식 라우트들이 랜더링된다 */}
-         <div className="flex items-end justify-center max-w-[1300px]">{/* <Footer /> */}</div>
+         <div className="flex items-end justify-center max-w-[1500px]">{/* <Footer /> */}</div>
       </div>
    );
 }
