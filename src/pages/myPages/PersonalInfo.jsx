38216c3b75f07253ce83b29e2fea1bde7942beb1
CHOI JAHYUN <0912jh@gmail.com>
2023-11-22 19:43:38 +0900

    ARTX-75 mypage 내  개인정보페이지 데이터연동

--- src/pages/myPages/PersonalInfo.jsx
+++ src/pages/myPages/PersonalInfo.jsx
@@ -6,16 +6,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import customAxios from '../../store/customAxios';
 import axios from 'axios';
+import MyPageDataFetcher from '../../components/shared/MyPageDataFetcher';
 
 // 개인정보관리
 // 이미지를 저장하면 서버로 전송된다
 // 이미지를 보여줄때 서버에서 가져온다
 
-const Label = ({ text }) => {
-    return <label className="bg-white w-20 px-2 border-b">{text}</label>;
+const Label = ({ text, value }) => {
+    return <label className="bg-white w-20 px-2 border-b" value={value} >{text}</label>;
 };
-const Input = ({ type }) => {
-    return <input className=" bg-white w-[150px] focus:outline-none px-4 border-b" type={type}></input>;
+const Input = ({ type, value, onChange, disable }) => {
+    return <input className=" bg-white w-[150px] focus:outline-none px-4 border-b" type={type} value={value} onChange={onChange} disable={disable}></input>;
 };
 const Button = ({ text }) => {
     return <button className=" bg-white w-13 px-4 mx-4 border italic rounded-sm hover:bg-gray-800 ">{text}</button>;
@@ -27,10 +28,30 @@ const Div = ({ children }) => {
 
 function PersonalInfo() {
     const localUsername = localStorage.getItem('username');
-    const userInfo = useSelector((state) => state.user);
+    // const userInfo = useSelector((state) => state.user);
     const navigate = useNavigate();
     // const profileImage = instance.post(`/api/users/{${userInfo.userId}}/image`)
     const fileInputRef = useRef(null);
+    const [ userData, setUserData ] = useState({}) 
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await MyPageDataFetcher();
+                if (data) {
+                    setUserData(data)
+                }
+            } catch (error) {
+                // Handle error
+                console.error('Failed to fetch data', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    console.log('userData',userData);
 
     const [imageUrl, setImageUrl] = useState(
         'https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMTgy/MDAxNjA0MjI4ODc1NDMw.Ex906Mv9nnPEZGCh4SREknadZvzMO8LyDzGOHMKPdwAg.ZAmE6pU5lhEdeOUsPdxg8-gOuZrq_ipJ5VhqaViubI4g.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%95%98%EB%8A%98%EC%83%89.jpg?type=w800'
@@ -65,7 +86,6 @@ function PersonalInfo() {
         //         console.error('No token found');
         //         return;
         //     }
-
         //     const res = await axiosInstance.get(`/api/mypage`);
         //     console.log('겟res', res.data);
         //     setImageUrl(
@@ -77,9 +97,27 @@ function PersonalInfo() {
         // }
     };
 
-    useEffect(() => {
-        fetchProfileInfo();
-    }, []); // Run on component mount
+    // useEffect(() => {
+    //     fetchProfileInfo();
+    // }, []); // Run on component mount
+
+    //화면 글자변경 함수
+    const handleTextChange = (e, field) => {
+        setUserData({ ...userData, [field]: e.target.value });
+
+    };
+
+    //업데이트된 유저데이터를 서버로 저장하는 버튼에 연결할 함수
+    // const handleSaveChanges = async () => {
+    //     try {
+    //         const res = await customAxios.put('/api/mypage', userData); // Adjust the API endpoint and method according to your backend
+    //         console.log('Save response:', res.data);
+    //         // Optionally, show a success message or update state
+    //     } catch (error) {
+    //         console.error('Error saving changes', error);
+    //         // Optionally, show an error message
+    //     }
+    // };
 
     return (
         <div className="bg-white text-black w-[1000px] flex justify-center pb-20">
@@ -107,26 +145,28 @@ function PersonalInfo() {
                 <div className="bg-white ml-6">
                     <Div>
                         <Label text="아이디" />
-                        <Input type="text" />
+                        <Input type="text" disable value={userData.username || ''} />
+                        {/* <div type="text" className=" bg-white w-[150px] focus:outline-none px-4 border-b" >{userData.username || ''}</div> */}
+                        
                     </Div>
                     <Div>
                         <Label text="이름" />
-                        <Input type="text" />
+                        <Input type="text" value={userData.userNickname || ''} onChange={handleTextChange}/>
                         <Button text="변경" />
                     </Div>
                     <Div>
                         <Label text="이메일" />
-                        <Input type="text" />
+                        <Input type="text" value={userData.userEmail || ''} onChange={handleTextChange}/>
                         <Button text="변경" />
                     </Div>
                     <Div>
                         <Label text="닉네임" />
-                        <Input type="text" />
+                        <Input type="text" value={userData.userNickname || ''} onChange={handleTextChange}/>
                         <Button text="변경" />
                     </Div>
                     <Div>
                         <Label text="전화번호" />
-                        <Input type="text" />
+                        <Input type="text" value={userData.userPhoneNumber || ''} onChange={handleTextChange}/>
                         <Button text="변경" />
                     </Div>
                 </div>
@@ -139,8 +179,12 @@ function PersonalInfo() {
                         <textarea
                             type="text"
                             className="w-[230px] focus:outline-none px-2 border-b resize-none bg-white"
-                        ></textarea>
-                        <Button text="변경" />
+                            value={userData.userIntroduction || ''} //내용없으면 빈문자열처리
+                            onChange={(e) => handleTextChange(e, 'userIntroduction')}
+                            ></textarea>
+                        <Button text="변경" 
+                        // onClick={handleSaveChanges}
+                        />
                     </Div>
                 </div>
             </div>
