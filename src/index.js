d38a4b635d7c1614b2fbdefb77812bf5f1dbb0d7
dlworhd <dlwodud821@gmail.com>
2023-11-23 01:28:53 +0900

    ARTX-118 feat: Add Channel IO API

--- src/index.js
+++ src/index.js
@@ -5,9 +5,23 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store'; // 여기서 store를 import 해야 함
+import ChannelService from './utils/ChannelService';
+import { REACT_APP_CHANNEL_TALK_KEY } from './utils/env';
+
+
 export * from "./utils/env";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const pk = REACT_APP_CHANNEL_TALK_KEY
+const pluginKey = pk;
+
+ChannelService.loadScript();
+ChannelService.boot({
+    pluginKey: pluginKey, // fill your plugin key
+});
+
+
 root.render(
   <BrowserRouter>
     <Provider store={store}>
